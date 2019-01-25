678d238b204a834a0d387739351ad8715458597c
Brandon Mowat <me@brandonmowat.com>
2019-01-25 17:31:57 -0500

    Adds API class to script

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Add event handlers and API requests
+
+// Constants
+
+class API {
+  constructor() {
+    this.url = "https://damp-plateau-89390.herokuapp.com/api/v1";
+  }
+
+  get(path, callback = function() {}) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        callback(xmlHttp.responseText);
+      }
+    }
+    xmlHttp.open("GET", `${this.url}${path}`, true); // true for asynchronous 
+    xmlHttp.send(null);
+  }
+
+}
+
+const api = new API();
+
+api.get('', )
