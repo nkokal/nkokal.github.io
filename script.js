9dd0efdb4552d094912f690be2c4a39ce01b256e
Brandon Mowat <me@brandonmowat.com>
2019-01-25 18:10:12 -0500

    Adds form submit button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
-// Constants
-
+/**
+ * API
+ * this is a class that will house our connections to the api
+ */
 class API {
   constructor() {
     this.url = "https://damp-plateau-89390.herokuapp.com/api/v1";
@@ -17,8 +19,24 @@ class API {
     xmlHttp.send(null);
   }
 
+  post(path, body, callback = function() {}) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        callback(xmlHttp.responseText);
+      }
+    }
+    xmlHttp.open("POST", `${this.url}${path}`, true); // true for asynchronous 
+    xmlHttp.send(body);
+  }
+
 }
 
 const api = new API();
 
-api.get('', )
+
+function submitTextData() {
+  const payload = { text: document.querySelector(".textInputBox").value };
+
+  api.post("/api/v1/data", JSON.stringify(payload));
+}
